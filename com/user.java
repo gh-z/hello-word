cf946b84fcca8497ed423edbb8f47dd6e675c8ba
gh-z <15046642783@163.com>
2018-03-19 16:11:55 +0800

    33

--- com/user.java
+++ com/user.java
@@ -5,13 +5,5 @@ public class user {
       System.out.print("之后");
     }
 
-    public void tradeDownloadServer() throws Exception {
-        System.out.print("之前");
-        byte[] bytes = new byte[1024];
-        System.out.print("之后");
-    }
-    public void a{
-        System.out.print("之前");
-    }
 
 }
