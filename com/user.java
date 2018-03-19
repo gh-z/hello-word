f9cbe12314c7cc3da60a79e3677ba0a20dd87c66
gh-z <15046642783@163.com>
2018-03-19 13:46:10 +0800

    22

--- com/user.java
+++ com/user.java
@@ -1,21 +1,17 @@
-public class user{
-  public void tradeDownload() throws Exception {
-    System.out.print("之前");
-     byte[] bytes = new byte[1024];
-    System.out.print("之后");
-  }
+public class user {
+    public void tradeDownload() throws Exception {
+      System.out.print("之前");
+      byte[] bytes = new byte[1024];
+      System.out.print("之后");
+    }
 
     public void tradeDownloadServer() throws Exception {
       System.out.print("之前");
-        System.out.print("之前");
-        byte[] bytes = new byte[1024];
-        System.out.print("之后");
+      byte[] bytes = new byte[1024];
+      System.out.print("之后");
     }
 
-<<<<<<< HEAD
-  public void tradeEdit() throws Exception {
-
-  }
-=======
->>>>>>> 9e97d8b7a5ad6d5d780778b8eb32782d79ad90cc
+    public void tradeEdit() throws Exception {
+      System.out.print("之前");
+    }
 }
