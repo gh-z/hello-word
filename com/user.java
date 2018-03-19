072304dbc3ee38335a8673e8cdaa2dfb861b9c55
gh-z <15046642783@163.com>
2018-03-19 11:31:33 +0800

    1

--- com/user.java
+++ com/user.java
@@ -1,8 +1,5 @@
 public class user{
-<<<<<<< HEAD
-=======
-  private  int 1;
-  private  int 2;
-  private  int 3;
->>>>>>> de893c9154e4f9756a5753712e3dfe9ba2e4b438
+  public void tradeDownload) throws Exception {
+    byte[] bytes = new byte[1024];
+  }
 }
