815a257b1909b052b12c6e3215cdffb1f7236f11
gh-z <15046642783@163.com>
2018-03-19 11:39:45 +0800

    11

--- com/user.java
+++ com/user.java
@@ -1,9 +1,15 @@
 public class user{
   public void tradeDownload() throws Exception {
-    byte[] bytes = new byte[1024];
+    System.out.print("之前");
+     byte[] bytes = new byte[1024];
+    System.out.print("之后");
   }
 
   public void tradeDelete() throws Exception {
-    System.out.print("删除");
+     System.out.print("删除");
+  }
+
+  public void tradeEdit() throws Exception {
+    System.out.print("编辑");
   }
 }
